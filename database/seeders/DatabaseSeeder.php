70a15618091feaa37cc1bab77a0d1841a7038169
m.malekanmehrayan <m.malekan@mehrayan.com>
2024-12-20 19:21:00 +0330

    test all

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Product;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-//        User::factory()->create();
+        User::factory()->create();
         Product::factory(20)->create();
     }
 }
